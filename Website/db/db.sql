645facf41ea52786edef74def88001299ad321ea
ezreen-san <ezrinheyqal@gmai.com>
2025-04-19 04:32:15 +0800

    updated the db.sql.

--- Website/db/db.sql
+++ Website/db/db.sql
@@ -1,23 +1,19 @@
 -- Create the database
-CREATE DATABASE IF NOT EXISTS breyer_college;
+CREATE DATABASE IF NOT EXISTS breyer_gombak;
 
 -- Use the database
-USE breyer_college;
+USE breyer_gombak;
 
 -- Create the users table
 CREATE TABLE IF NOT EXISTS users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(50) UNIQUE NOT NULL,
-    email VARCHAR(50) UNIQUE NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  username VARCHAR(50) UNIQUE NOT NULL,
+  email VARCHAR(50) UNIQUE NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  reset_token_hash VARCHAR(64) UNIQUE NULL DEFAULT NULL,
+  reset_token_expires_at DATETIME NULL DEFAULT NULL,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
-
--- Add these columns to the users table to support password reset functionality
-ALTER TABLE users
-  ADD `reset_token_hash` VARCHAR(64) NULL DEFAULT NULL,
-  ADD `reset_token_expires_at` DATETIME NULL DEFAULT NULL,
-  ADD UNIQUE (`reset_token_hash`);
 
 -- Create the admins table
 CREATE TABLE IF NOT EXISTS admins (
